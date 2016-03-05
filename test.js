10a2172254ba8f5c9732f0628b719e0211a7c541
Martin Bories <martin.bories@megatherium.solutions>
2016-03-05 18:14:20 +0100

    Update test.js
    Added test case.

--- test.js
+++ test.js
@@ -7,3 +7,4 @@ console.log(parse({ message: 'something else entirely' }));
 console.log(parse({ message: 'E11000 duplicate key error index: stompflow.members.$email_1  dup key' }));
 console.log(parse({ message: 'insertDocument :: caused by :: 11000 E11000 duplicate key error index: pvn.users.$_email_1  dup key: { : "j@b.com" }' }));
 console.log(parse({ message: 'E11000 duplicate key error index: stompflow-test.members.$email_1  dup key' }));
+console.log(parse({message: 'E11000 duplicate key error collection: black-back-space.users index: username_1 dup key: { : "SargTeX" }'}));
