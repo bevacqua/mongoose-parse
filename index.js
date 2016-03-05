c685d138a8fdca78b54e2355e72b0da95bc9005c
Martin Bories <martin.bories@megatherium.solutions>
2016-03-05 18:13:16 +0100

    Update index.js
    We had another problem when using mongoose and mongoose-parse under node 4.6.1 (LTS).
    The code that worked for 5.x didn't work here, as the error response from mongoose was in a different format, example:
    `'E11000 duplicate key error collection: .users index: username_1 dup key: { : "theusername" }'`
    
    The code above fixes this.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var parts = /E11000 duplicate key error index: (?:[a-z-_]+\.)+\$_?([a-z]+)/;
+var parts = /^E11000 duplicate key error (index|collection): (?:[a-z-.]+\.)+([a-z]*) index\: ([a-z]+)_([0-9]+)/;
 
 function parse (err) {
   var matches = err && err.message.match(parts);
   if (matches) {
-    return { code: 11000, path: matches[1] };
+    return { code: 11000, path: matches[3] };
+  } else {
+        var matches = err && err.message.match(/E11000 duplicate key error index: (?:[a-z-_]+\.)+\$_?([a-z]+)/);
+        if (matches) {return {code: 11000, path: matches[1]}; }
   }
   return err;
 }
